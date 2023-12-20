1c5531aea591f60b7e02e30dad00aeb047580aa7
MoMagdy <hamomagdy12266@gmail.com>
2023-12-20 13:32:20 +0200

    first commit

---
+++ MainLogic.js
@@ -0,0 +1,265 @@
+function homeUI() {
+    let token = localStorage.getItem("token")
+    let logindiv = document.getElementById("login-div")
+    let logoutBtn = document.getElementById("logout-div")
+    const addPostBtn = document.getElementById("creatPost-btn")
+    getName()
+
+    if (token == null) {
+
+        if (addPostBtn != null) {
+            addPostBtn.style.setProperty("display", "none", "important")
+        }
+        logindiv.style.setProperty("display", "flex", "important")
+        logoutBtn.style.setProperty("display", "none", "important")
+
+    } else {
+        if (addPostBtn != null) {
+            addPostBtn.style.setProperty("display", "none", "important")
+
+        }
+        logoutBtn.style.setProperty("display", "flex", "important")
+        logindiv.style.setProperty("display", "none", "important")
+
+        getName()
+
+
+    }
+
+}
+
+function getName() {
+    let user = ""
+
+    const userName = localStorage.getItem("username")
+    const image = localStorage.getItem("image")
+    if (userName != null) {
+        document.getElementById("userName").innerHTML = userName
+        document.getElementById("user-image").src = image
+
+    }
+
+
+}
+function createNewPost() {
+    let title = document.getElementById("Title").value
+    let content = document.getElementById("postContent").value
+    let image = document.getElementById("Image").files[0]
+
+    let formData = new FormData()
+    formData.append("body", content)
+    formData.append("title", title)
+    formData.append("image", image)
+
+    let token = localStorage.getItem("token")
+    let header = {
+        "authorization": `Bearer ${token}`
+    }
+    axios.post("https://tarmeezacademy.com/api/v1/posts", formData, { headers: header })
+        .then((response) => {
+            console.log(response)
+
+            let modal = document.getElementById("newPost-modal")
+            let modalInstance = bootstrap.Modal.getInstance(modal)
+            modalInstance.hide()
+            showAlerts("Posted successfully!!", "success")
+            getPost()
+        }).catch(error => {
+
+            alert(error)
+        })
+
+}
+function showAlerts(messagealert, Type) {
+    const alertPlaceholder = document.getElementById("alert")
+    const appendAlert = (message, type) => {
+        const wrapper = document.createElement('div')
+        wrapper.innerHTML = [
+            `<div class="alert alert-${Type} alert-dismissible" role="alert"  >`,
+            `   <div>${messagealert}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+
+            '</div>'
+        ].join('')
+
+        alertPlaceholder.append(wrapper)
+    }
+    appendAlert(messagealert, 'success')
+
+    //TODO setTimeout(() => {
+
+    //     const alert = bootstrap.Alert.getOrCreateInstance('#success-alert')
+    //     alert.close()
+    // }, 3000)
+
+}
+
+
+function registerNewUser() {
+
+    console.log("meaw")
+    let userName = document.getElementById("register-username").value
+    let name = document.getElementById("register-name").value
+    let email = document.getElementById("register-email").value
+    let password = document.getElementById("password").value
+    let image = document.getElementById("register-image").files[0]
+
+    let formData = new FormData()
+    formData.append("username", userName)
+    formData.append("name", name)
+    formData.append("emali", email)
+    formData.append("password", password)
+    formData.append("image", image)
+
+    let header = {
+        "Content-Type": "multipart/form-data"
+    }
+
+
+    axios.post(`https://tarmeezacademy.com/api/v1/register`, formData, { headers: header })
+        .then((response) => {
+
+            let token = response.data.token
+            localStorage.setItem("token", token)
+
+            console.log(response)
+
+            let modal = document.getElementById("Register-modal")
+            let modalInstance = bootstrap.Modal.getInstance(modal)
+            modalInstance.hide()
+            showAlerts("Register In Successfully!", "success")
+
+            homeUI()
+
+
+
+        }).catch(erroe => {
+            showAlerts(erroe.response.data.message, "danger")
+        })
+
+}
+
+function Login() {
+    let username = document.getElementById("username").value
+    let password = document.getElementById("password").value
+    let params =
+    {
+        "username": username,
+        "password": password
+    }
+
+    axios.post("https://tarmeezacademy.com/api/v1/login", params)
+        .then((response) => {
+            let token = response.data.token
+            let uesername = response.data.user.username
+            let image = response.data.user.profile_image
+
+            localStorage.setItem("token", token)
+            localStorage.setItem("image", image)
+            localStorage.setItem("username", username)
+
+            console.log(response)
+            let modal = document.getElementById("login-modal")
+            let modalInstance = bootstrap.Modal.getInstance(modal)
+            modalInstance.hide()
+            showAlerts("Logged In Successfully!", "success")
+            homeUI()
+
+        }).catch(error => {
+            alert(error)
+        })
+
+
+}
+function logout() {
+    localStorage.removeItem("token")
+    showAlerts("Logged Out Successfully!", "success");
+    homeUI()
+}
+function getPost(page) {
+
+
+
+    axios.get(`https://tarmeezacademy.com/api/v1/posts?limit=5&page=${page}`)
+        .then((response) => {
+            let posts = response.data.data
+            lastPage = response.data.meta.last_page
+            console.log(posts)
+
+
+            for (post of posts) {
+
+                let postTitle = ""
+                if (!post.title == null) {
+
+                    postTitle = posttitle
+                }
+
+                let content =
+                    `
+
+    <div class="card shadow p-3 my-5 " style = "cursor: pointer;" onClick ="postClicked(${post.id})">
+                <div class="card-header">
+                <img src="${post.image}" alt="" class="rounded-circle border border-3"
+                style="width: 50px; height: 50px;">
+                <h1 class="d-inline fs-4">@${post.author.username}</h1>
+                </div>
+
+            <div class="card-body">
+                <img src="${post.image}" alt="" class="w-100" >
+
+                <p id="timeUpload">${post.created_at}</p>
+                <h4 class="fs-4">
+                ${post.title}
+                </h4>
+                <p>${post.body}</p>
+                <hr>
+           <div>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                     class="bi bi-chat-text" viewBox="0 0 16 16">
+                    <path
+                        d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894m-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
+                     <path
+                        d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5" />
+                </svg>
+                <span>
+                    (${post.comments_count}) comments
+                    <span id = "post-tag-${post.id}">
+                        <button type="button" class="btn btn-outline-secondary">${post.tags}</button>
+                        
+                    </span>
+
+                </span>
+         </div>
+    </div>
+                
+        `
+
+                document.getElementById("posts").innerHTML += content
+                const currentPostIdTags = `post-tag-${post.id}`
+
+                document.getElementById(currentPostIdTags).innerHTML = ""
+
+                for (tag of post.tags) {
+                    console.log(tag)
+
+                    let content =
+                        `
+                    
+                       
+                          <button type="button" class="btn btn-outline-secondary rounded-5" style="margin: 5px;">${tag.name} hh</button>
+
+                    `
+                    document.getElementById(currentPostIdTags).innerHTML += content
+
+                }
+
+
+            }
+
+        })
+}
+function Home() {
+    window.location = "Home.html"
+}
+
