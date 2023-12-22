c161280e208e03ff3018321888fe2a0b80e51628
MoMagdy <hamomagdy12266@gmail.com>
2023-12-22 22:29:16 +0200

    Finish-Ptoject

--- MainLogic.js
+++ MainLogic.js
@@ -1,3 +1,69 @@
+// handle scrolling //
+let currentPage = 1
+let lastPage = 1
+window.addEventListener("scroll", () => {
+
+    let endOfPage = window.innerHeight + window.pageYOffset >= document.body.scrollHeight;
+
+    console.log(currentPage, lastPage)
+    if (endOfPage && currentPage < lastPage) {
+
+        currentPage = currentPage + 1
+
+        getPosts(false, currentPage)
+
+    }
+});
+// handle scrolling //
+
+
+function createNewPost() {
+    let postId = document.getElementById("post-id-input").value
+    let isCreat = postId == null || postId == ""
+
+
+    let title = document.getElementById("Title").value
+    let content = document.getElementById("postContent").value
+    let image = document.getElementById("Image").files[0]
+
+    let formData = new FormData()
+    formData.append("body", content)
+    formData.append("title", title)
+    formData.append("image", image)
+
+    let token = localStorage.getItem("token")
+    let header = {
+        "authorization": `Bearer ${token}`
+    }
+    let url = ``
+    if (isCreat) {
+        url = "https://tarmeezacademy.com/api/v1/posts"
+    } else {
+        formData.append("_method", "put")
+        url = `https://tarmeezacademy.com/api/v1/posts/${postId}`
+
+
+    }
+    toggleLoder(true)
+    axios.post(url, formData, { headers: header })
+        .then((response) => {
+            console.log(response)
+
+            let modal = document.getElementById("new-Post-modal")
+            let modalInstance = bootstrap.Modal.getInstance(modal)
+            modalInstance.hide()
+            showAlerts("Posted successfully!!", "success")
+            getPosts()
+        }).catch(error => {
+
+            showAlerts(erroe.response.data.message, "danger")
+        }).finally(() => {
+            toggleLoder(false)
+        })
+
+
+}
+
 function homeUI() {
     let token = localStorage.getItem("token")
     let logindiv = document.getElementById("login-div")
@@ -41,14 +107,12 @@ function showAlerts(messagealert, Type) {
             `<div class="alert alert-${Type} alert-dismissible" role="alert"  >`,
             `   <div>${messagealert}</div>`,
             '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-
             '</div>'
         ].join('')
 
         alertPlaceholder.append(wrapper)
     }
     appendAlert(messagealert, 'success')
-
     //TODO setTimeout(() => {
 
     //     const alert = bootstrap.Alert.getOrCreateInstance('#success-alert')
@@ -75,8 +139,7 @@ function registerNewUser() {
     let header = {
         "Content-Type": "multipart/form-data"
     }
-
-
+    toggleLoder(true)
     axios.post(`https://tarmeezacademy.com/api/v1/register`, formData, { headers: header })
         .then((response) => {
 
@@ -89,13 +152,11 @@ function registerNewUser() {
             let modalInstance = bootstrap.Modal.getInstance(modal)
             modalInstance.hide()
             showAlerts("Register In Successfully!", "success")
-
             homeUI()
-
-
-
         }).catch(erroe => {
             showAlerts(erroe.response.data.message, "danger")
+        }).finally(() => {
+            toggleLoder(false)
         })
 
 }
@@ -109,6 +170,7 @@ function Login() {
         "password": password
     }
 
+    toggleLoder(true)
     axios.post("https://tarmeezacademy.com/api/v1/login", params)
         .then((response) => {
             let token = response.data.token
@@ -127,13 +189,17 @@ function Login() {
             homeUI()
 
         }).catch(error => {
-            alert(error)
+            showAlerts(erroe.response.data.message, "danger")
+        }).finally(() => {
+            toggleLoder(false)
+
         })
 
 
 }
 function logout() {
     localStorage.removeItem("token")
+    localStorage.removeItem("user")
     showAlerts("Logged Out Successfully!", "success");
     homeUI()
 }
@@ -142,9 +208,8 @@ function Home() {
     window.location = "home.html"
 }
 
-function Profile(){
-    window.location = "profile.html"
-
+function profileClicked(postId) {
+    window.location = `profile.html?POST=${postId}`
 }
 function getCurrentUser() {
     let user = ""
@@ -152,33 +217,32 @@ function getCurrentUser() {
     if (user != null) {
         user = JSON.parse(userStorage)
     }
-
     return user
-
 }
-function getPosts(page) {
+
+
+
+function getPosts(realod = false, page) {
+    toggleLoder(true)
     axios.get(`https://tarmeezacademy.com/api/v1/posts?limit=5&page=${page}`)
         .then((response) => {
+            toggleLoder(false)
             let posts = response.data.data
             lastPage = response.data.meta.last_page
+
             console.log(posts)
-
-
             for (post of posts) {
-
                 let postTitle = ""
-
                 // show or hide edit button //
                 let user = getCurrentUser()
                 let isMyPost = user != null && post.author.id == user.id
                 let editButtonContent = ``
                 if (isMyPost) {
                     editButtonContent =
-                        `      
-                   
-                <button onclick="deletePostClicked('${encodeURIComponent(JSON.stringify(post))}')" type="button" class="btn btn-outline-danger mx-2" style = "float: inline-end; margin-right = 10px ;" id="delete-btn">Delete</button>
-                 <button onclick="EditPost('${encodeURIComponent(JSON.stringify(post))}')" type="button" class="btn btn-outline-secondary mx-2" style = "float: inline-end;" id="edit-btn">Edit</button>
-                `
+                        `
+                        <button onclick="deletePostClicked('${encodeURIComponent(JSON.stringify(post))}')" type="button" class="btn btn-outline-danger mx-2" style = "float: inline-end; margin-right = 10px ;" id="delete-btn">Delete</button>
+                        <button onclick="EditPost('${encodeURIComponent(JSON.stringify(post))}')" type="button" class="btn btn-outline-secondary mx-2" style = "float: inline-end;" id="edit-btn">Edit</button>
+                        `
                 }
                 if (!post.title == null) {
 
@@ -186,50 +250,55 @@ function getPosts(page) {
                 }
                 let content =
                     `
-
-    <div class="card shadow p-3 my-5 " style = "cursor: pointer;" >
-                <div class="card-header">
-                <img src="${post.image}" alt="" class="rounded-circle border border-3"
-                style="width: 50px; height: 50px;">
-                <h1 class="d-inline fs-4">@${post.author.username}</h1>
-
-                ${editButtonContent}
+             <div class="card shadow p-3 my-5 " style="cursor: pointer;">
+                <div class="card-header" >
+                  <span onClick="profileClicked(${post.author.id})"  class = "col-3">
+                    <img src="${post.image}" alt="" class="rounded-circle border border-3" style="width: 50px; height: 50px;">
+                    <h1 class="d-inline fs-4">@${post.author.username}</h1>
+                  </span>
+                        ${editButtonContent}
                 </div>
+                 
+            
+                    <div class="card-body" onClick="postClicked(${post.id})">
+                        <img src="${post.image}" alt="" class="w-100">
+                        <p id="timeUpload">${post.created_at}</p>
+                        <h4 class="fs-4">
+                            ${post.title}
+                        </h4>
+                        <p>${post.body}</p>
+                        <hr>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-chat-text" viewBox="0 0 16 16">
+                                <path
+                                    d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894m-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
+                                <path
+                                    d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5" />
+                            </svg>
+                            <span>
+                                (${post.comments_count}) comments
+                                <span id="post-tag-${post.id}">
+                                    <button type="button" class="btn btn-outline-secondary">${post.tags}</button>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            
 
-            <div class="card-body" onClick ="postClicked(${post.id})">
-                <img src="${post.image}" alt="" class="w-100" >
-
-                <p id="timeUpload">${post.created_at}</p>
-                <h4 class="fs-4">
-                ${post.title}
-                </h4>
-                <p>${post.body}</p>
-                <hr>
-           <div>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                     class="bi bi-chat-text" viewBox="0 0 16 16">
-                    <path
-                        d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894m-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
-                     <path
-                        d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5" />
-                </svg>
-        <span>
-          (${post.comments_count}) comments
-              <span id = "post-tag-${post.id}">
-                     <button type="button" class="btn btn-outline-secondary">${post.tags}</button> 
-              </span>
-        </span>
-    </div>
-                
-        `
+                    `
 
                 document.getElementById("posts").innerHTML += content
                 const currentPostIdTags = `post-tag-${post.id}`
+                document.getElementById(currentPostIdTags).innerHTML = ""
+
                 for (tag of post.tags) {
                     console.log(tag)
 
                     let content =
-                        ` <button type="button" class="btn btn-outline-secondary rounded-5" style="margin: 5px;">${tag.name} hh</button>
+                        ` 
+                     <button type="button" class="btn btn-outline-secondary rounded-5" style="margin: 5px;">${tag.name} hh</button>
                     `
                     document.getElementById(currentPostIdTags).innerHTML += content
                 }
@@ -239,4 +308,86 @@ function getPosts(page) {
 
         })
     homeUI()
+}
+function postClicked(postId) {
+
+    window.location = `postDetails.html?POST=${postId}`
+
+}
+
+function deletePostClicked(postObj) {
+    let post = JSON.parse(decodeURIComponent(postObj))
+    let postId = document.getElementById("post-id").value = post.id
+
+    let postModal = new bootstrap.Modal(document.getElementById("deletePost-modal"))
+    postModal.toggle()
+}
+
+
+function deletePost() {
+
+    let postId = document.getElementById("post-id").value
+    let token = localStorage.getItem("token")
+    let headers = {
+        "Authorization": `Bearer ${token}`
+    }
+
+    axios.delete(`https://tarmeezacademy.com/api/v1/posts/${postId}`, { headers: headers })
+        .then((response) => {
+
+            console.log(response)
+            let modal = document.getElementById("deletePost-modal")
+            let modalInstance = bootstrap.Modal.getInstance(modal)
+            modalInstance.hide()
+            showAlerts("Deleted Successfully!", "success")
+            homeUI()
+            getPosts()
+
+
+        }).catch(error => {
+            showAlerts(erroe.response.data.message, "danger")
+        })
+
+
+}
+
+
+function createNewPostClicked() {
+
+
+    document.getElementById("post-id-input").value = ""
+    document.getElementById("modal-post-title").innerHTML = "Creat A New Post"
+    document.getElementById("post-btn").innerHTML = "Creat"
+    document.getElementById("Title").value = ""
+    document.getElementById("postContent").value = ""
+
+
+    let postModal = new bootstrap.Modal(document.getElementById("new-Post-modal"))
+    postModal.toggle()
+}
+
+function EditPost(postObj) {
+    let post = JSON.parse(decodeURIComponent(postObj))
+
+    document.getElementById("Post-btn").innerHTML = "Update"
+    document.getElementById("post-id-input").value = post.id
+    document.getElementById("Modal-post-title").innerHTML = "Edit Post"
+    document.getElementById("Title").value = post.title
+    document.getElementById("postContent").value = post.body
+    let postModal = new bootstrap.Modal(document.getElementById("new-Post-modal"))
+    postModal.toggle()
+}
+function myProfileClicked() {
+
+    user = getCurrentUser()
+    window.location = `profile.html?POST=${user.id}`
+}
+
+function toggleLoder(show = true) {
+
+    if (show) {
+        document.getElementById("loder").style.visibility = 'visible'
+    } else {
+        document.getElementById("loder").style.visibility = 'hidden'
+    }
 }
--- profileScripts.js
+++ profileScripts.js
@@ -1,1 +1,116 @@
 homeUI()
+function getCurrentUserId() {
+    const url = new URLSearchParams(window.location.search)
+    userId = url.get("POST")
+
+    return userId
+
+}
+
+function getUser() {
+
+    const userId = getCurrentUserId()
+
+    axios.get(`https://tarmeezacademy.com/api/v1/users/${userId}`)
+        .then((response) => {
+
+            console.log(response)
+
+            let user = response.data.data
+            document.getElementById("profile-image").src = user.profile_image
+            document.getElementById("main-info-name").innerHTML = "Name: " + user.name
+            document.getElementById("main-info-email").innerHTML = "Email: " + user.email
+            document.getElementById("main-info-username").innerHTML = "User-name: " + user.username
+            document.getElementById("user-name").innerHTML = user.username + " Post's"
+
+            document.getElementById("no-comment").innerHTML = user.comments_count
+            document.getElementById("no-posts").innerHTML = user.posts_count
+        })
+}
+
+
+function getPosts() {
+    const userId = getCurrentUserId()
+
+    axios.get(`https://tarmeezacademy.com/api/v1/users/${userId}/posts`)
+        .then((response) => {
+            let posts = response.data.data
+            console.log(posts)
+            for (post of posts) {
+                let postTitle = ""
+                // show or hide edit button //
+                let user = getCurrentUser()
+                let isMyPost = user != null && post.author.id == user.id
+                let editButtonContent = ``
+                if (isMyPost) {
+                    editButtonContent =
+                        `
+                        <button onclick="deletePostClicked('${encodeURIComponent(JSON.stringify(post))}')" type="button" class="btn btn-outline-danger mx-2" style = "float: inline-end; margin-right = 10px ;" id="delete-btn">Delete</button>
+                        <button onclick="EditPost('${encodeURIComponent(JSON.stringify(post))}')" type="button" class="btn btn-outline-secondary mx-2" style = "float: inline-end;" id="edit-btn">Edit</button>
+                        `
+                }
+                if (!post.title == null) {
+
+                    postTitle = posttitle
+                }
+                let content =
+                    `
+             <div class="card shadow p-3 my-5 " style="cursor: pointer; id = "posts"">
+                    <div class="card-header" >
+                        <img src="${post.image}" alt="" class="rounded-circle border border-3" style="width: 50px; height: 50px;">
+                        <h1 class="d-inline fs-4">@${post.author.username}</h1>
+                        ${editButtonContent}
+                    </div>
+            
+                    <div class="card-body" onClick="postClicked(${post.id})">
+                        <img src="${post.image}" alt="" class="w-100">
+                        <p id="timeUpload">${post.created_at}</p>
+                        <h4 class="fs-4">
+                            ${post.title}
+                        </h4>
+                        <p>${post.body}</p>
+                        <hr>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-chat-text" viewBox="0 0 16 16">
+                                <path
+                                    d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894m-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
+                                <path
+                                    d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8m0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5" />
+                            </svg>
+                            <span>
+                                (${post.comments_count}) comments
+                                <span id="post-tag-${post.id}">
+                                    <button type="button" class="btn btn-outline-secondary">${post.tags}</button>
+                                </span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+           
+            
+
+                    `
+
+                document.getElementById("posts").innerHTML += content
+                const currentPostIdTags = `post-tag-${post.id}`
+                document.getElementById(currentPostIdTags).innerHTML = ""
+
+                for (tag of post.tags) {
+                    console.log(tag)
+
+                    let content =
+                        ` 
+                     <button type="button" class="btn btn-outline-secondary rounded-5" style="margin: 5px;">${tag.name}</button>
+                    `
+                    document.getElementById(currentPostIdTags).innerHTML += content
+                }
+
+
+
+            }
+
+        })
+    homeUI()
+}
+
